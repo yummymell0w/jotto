8df3b5e4dfe13504ad79dbfc5a30f52f71d36de7
Margarita Artiushenko <margoart.work@gmail.com>
2020-05-17 20:59:24 -0700

    add getLEtterMatchCount helper function

---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord
+ * @param {string} secretWord
+ * @returns {number}
+ */
+export function getLetterMatchCount(guessedWord, secretWord) {
+    const secretLetterSet = new Set(secretWord.split(''));
+    const guessedLetterSet = new Set(guessedWord.split(''));
+    return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length;
+};
---
+++ src/helpers/index.test.js
@@ -0,0 +1,17 @@
+import {getLetterMatchCount} from "./index";
+
+describe('getLetterMatchCount', () => {
+    const secretWord = 'party';
+    test('returns correct count when there are no matching letters', () => {
+        const letterMatchCount = getLetterMatchCount('bones', secretWord);
+        expect(letterMatchCount).toBe(0);
+    });
+    test('return correct count when there are 3 matchign letter', () => {
+        const letterMatchCount = getLetterMatchCount('train', secretWord);
+        expect(letterMatchCount).toBe(3);
+    });
+    test('returns correct count when there are duplicate letters in the guess', () => {
+        const letterMatchCount = getLetterMatchCount('parka', secretWord);
+        expect(letterMatchCount).toBe(3);
+    });
+});
